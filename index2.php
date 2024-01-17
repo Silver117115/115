9fd53fad21da00c6403396659ba16fdbdef3868b
Silver117115 <hooa73982@gmail.com>
2024-01-17 14:36:50 -0600

    se sube un modelo de footer

--- index2.php
+++ index2.php
@@ -23,6 +23,7 @@ if(isset($_SESSION['usuario'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style2.css">
     <title>Yo no lo hago por que yo ya lo tengo: Cursos y más</title>
+    <script src="https://kit.fontawesome.com/3abdfc9b28.js" crossorigin="anonymous"></script>
 </head>
 <body>
 
@@ -57,17 +58,68 @@ if(isset($_SESSION['usuario'])){
         </div>
         
     </header>
+
     <main>
         <h3>¿Tu CV no es suficiente? ¿Estancado en tu   
             desarrollo de  personaje?
        </h3>       
     </main>
-    <section>
 
-    </section>
+    <div class="body_page">
+        <div class="cover"></div>
+        <div class="container_article">
+            <div class="box_article"></div>
+            <div class="box_article"></div>
+            <div class="box_article"></div>
+            <div class="box_article"></div>
+            <div class="box_article"></div>
+            <div class="box_article"></div>
+            <div class="box_article"></div>
+            <div class="box_article"></div>
+        </div>
+    </div>
+
     <footer>
+        <div class="container_footer">
+            <div class="box_footer">
+                <div class="logo">
+                    <img src="" alt="">
+                </div>
+                <div class="terms">
+                    <p>Esta es una pequeña prueba para el footer hijos de perra a ver si les gusta o comen salchicha</p>
+                </div>
+            </div>
+            <div class="box_footer">
+                <h2>Soluciones</h2>
+                <a href="#">¿Problemas con tus cursos?</a>
+                <a href="#">Consultas</a>
+                <a href="#">Aclaraciones</a>
+                <a href="#">¿Problemas con tu pago?</a>
+            </div>
+            <div class="box_footer">
+                <h2>Compañia</h2>
+                <a href="#">Acerca de</a>
+                <a href="#">Trabajos</a>
+                <a href="#">Procesos</a>
+                <a href="#">Servicios</a>
+            </div>
+            <div class="box_footer">
+                <h2>Redes sociales</h2>
+                <a href="#"><i class="fab fa-facebook-square"></i>Facebook</a>
+                <a href="#"><i class="fa-brands fa-square-x-twitter"></i>X</i></a>
+                <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+            </div>
+        </div>
+
+        <div class="box__copyright">
+            <hr>
+            <p>Todos los derechos reservados © 2024 <b>Zorras en servicio</b></p>
+        </div>
+
+
 
     </footer>
+
 
 </body>
 </html>
