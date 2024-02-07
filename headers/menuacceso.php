327d8b19ca0d934bbbf623cd6559502e221d4f2e
Eduardo Lutrillo <lalovaferoz117@gmail.com>
2024-02-07 07:43:22 -0600

    added main content

--- headers/menuacceso.php
+++ headers/menuacceso.php
@@ -96,10 +96,51 @@
             Dashboard
         </div>
         <section>
-        <h2>Aprende más... Prácticando más</h2>
-        <h3>Preparación desde Secundaria hasta una Especialización</h3>
-        
+            <h2>Aprende más... Prácticando más</h2>
+            <h3>Preparación Básica hasta una Especialización</h3>
+            <h2>Más Demandados</h2>
         </section>
+        <section class="container">
+        <article class="post">
+            <h4>Título de mi post</h4>
+            <p>Descripción de mi post</p>
+            <img src="img/secundaria.jpg" alt="cursosecundaria">
+            <p>Texto del artículo e información</p>
+        </article>
+        <article class="post">
+            <h4>Título de mi post 2</h4>
+            <p>Descripción de mi post 2</p>
+            <img src="img/bachillerato.jpg" alt="cursobachillerato">
+            <p>Texto del artículo e información</p>
+        </article>
+        <article class="post">
+            <h4>Título de mi post 3</h4>
+            <p>Descripción de mi post 3</p>
+            <img src="img/universidad.jpg" alt="cursouniversidad">
+            <p>Texto del artículo e información</p>
+        </article>
+        </section>
+        <aside>
+            <h4>Categorías</h4>
+            <div>
+                <h4>Tecnología</h4>
+                <p>Introducción a la programación</p>
+                <p>JavaScript</p>
+                <p>Desarrollo Web</p>
+            </div>
+            <div>
+                <h4>Educación Básica y Medio-Superior</h4>
+                <p>Primaria</p>
+                <p>Secundaria</p>
+                <p>Bachillerato</p>
+            </div>
+            <div>
+                <h4>Metodos de Aprendizaje</h4>
+                <p>Técnicas de Estudio</p>
+                <p>Meditación</p>
+                <p>Deja el Ciclo infinito del Estudiante</p>
+            </div>
+        </aside>
     
         <?php include "footer/footer.php"; ?>
     </section>
