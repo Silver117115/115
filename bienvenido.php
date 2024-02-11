d4227cfbcfb23a91c223bd8d3089c7352cc81b50
Eduardo Lutrillo <lalovaferoz117@gmail.com>
2024-02-10 20:34:03 -0600

    estilo-contenido

--- bienvenido.php
+++ bienvenido.php
@@ -23,7 +23,57 @@ if(isset($_SESSION['usuario'])){
 </head>
 <?php include "headers/menuacceso.php"; ?>
 
-<body id="body">
+<body>
+<section>
+        <h2>Aprende más... Prácticando más</h2>
+        <h3>Preparación Básica hasta una Especialización</h3>
+        <h2>Más Demandados</h2>
+</section>
+        <section class="container">
+        <article class="post">
+            <h4>Título de mi post</h4>
+            <p>Descripción de mi post</p>
+            <img class="img-curso" src="img/secundaria.jpg" alt="cursosecundaria" width="200px">
+            <p>Texto del artículo e información</p>
+        </article>
+        <article class="post">
+            <h4>Título de mi post 2</h4>
+            <p>Descripción de mi post 2</p>
+            <img class="img-curso" src="img/bachillerato.jpg" alt="cursobachillerato" width="200px">
+            <p>Texto del artículo e información</p>
+        </article>
+        <article class="post">
+            <h4>Título de mi post 3</h4>
+            <p>Descripción de mi post 3</p>
+            <img class="img-curso" src="img/universidad.jpg" alt="cursouniversidad" width="200px">
+            <p>Texto del artículo e información</p>
+        </article>
+        </section>
+        <aside>
+            <h4>Categorías</h4>
+            <div>
+                <h4>Tecnología</h4>
+                <p>Introducción a la programación</p>
+                <p>JavaScript</p>
+                <p>Desarrollo Web</p>
+            </div>
+            <div>
+                <h4>Educación Básica y Medio-Superior</h4>
+                <p>Primaria</p>
+                <p>Secundaria</p>
+                <p>Bachillerato</p>
+            </div>
+            <div>
+                <h4>Metodos de Aprendizaje</h4>
+                <p>Técnicas de Estudio</p>
+                <p>Meditación</p>
+                <p>Deja el Ciclo infinito del Estudiante</p>
+            </div>
+        </aside>
+        <?php include "footer/footer.php"; ?>
 
-</body>
+    
+
+    </body>
+
 </html>
