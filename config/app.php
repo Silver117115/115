40c591c904376c47e21819a52e44bf3785d6ec65
Silver117115 <hooa73982@gmail.com>
2024-02-12 18:03:56 -0600

    cambios

--- config/app.php
+++ config/app.php
@@ -1,8 +1,7 @@
 <?php
-    const APP_URL="http://localhost/115/";
-    const APP_NAME="115 CLONE UDEM";
-    const APP_SESSION_NAME=;"CRUD"
+    const APP_URL = "http://localhost/115/";
+    const APP_NAME = "115 CLONE UDEM";
+    const APP_SESSION_NAME = "CRUD";
 
-    date_Default_timezone_set("America/Mexico_City");
-
+    date_default_timezone_set("America/Mexico_City");
 ?>
